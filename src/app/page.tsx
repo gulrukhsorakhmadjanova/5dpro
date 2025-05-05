665ffd4f4d09d4e8d33de74c1a7628b6751803ef
Gulrukh Akhmad <akhmadjanovagulrukhsor@gmail.com>
2025-05-05 22:45:55 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function TodoApp() {
+  return (
+    <main className="p-4 max-w-md mx-auto">
+      <h1 className="text-2xl font-bold mb-4">My Todo List</h1>
+      {/* Add your todo components here */}
+    </main>
+  )
+}
